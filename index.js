3e6772c70c64b90f25da53743c0b0b4a74e6d503
michoel kahana <kmichoel25@gmail.com>
2022-12-14 22:58:44 -0500

    commit tree fa7efe608ab7a2deaff5f959b67447a44ded1372

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+const dodger = document.getElementById("dodger")
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+
+  if (left > 0) {
+    dodger.style.left = `${left - 10}px`;
+  }
+}
+
+function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+        const right = parseInt(rightNumbers, 10);
+    
+        if (right > 0) {
+          dodger.style.left = `${right + 10}px`;
+        }
+    }
+
+function moveDodgerUp() {
+    const upNumbers = dodger.style.left.replace("px", "");
+    const up = parseInt(upNumbers, 10);
+
+    if (up > 0) {
+      dodger.style.left = `${up - 10}px`;
+    }
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+
+document.addEventListener("keydown", function (e) {
+ if (e.key === "ArrowRight") {
+    moveDodgerRight();
+ }
+});
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowUp") {
+        moveDodgerUp();
+    }
+});
